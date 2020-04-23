035e10876f3d23d2de55a3bd2b84f58e57629d33
AbdulMalik <abdumaliksharipov@gmail.com>
2020-04-22 23:00:18 -0500

    Fine-tuning via on() event listener"

--- toDoListjQuery/assets/js/todos.js
+++ toDoListjQuery/assets/js/todos.js
@@ -1,15 +1,23 @@
 
 
-$('li').click(function(){
+$("ul").on("click","li",function(){
     $(this).toggleClass("completed");
 
 });
 
 
-$("span").click(function(e){
+$("ul").on("click","span",function(e){
     $(this).parent().fadeOut(1500,function(){
         $(this.remove());
     });
 
     e.stopPropagation();
 });
+
+$("input").keypress(function(e){
+    if(e.which===13){
+        let toDo=$(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> "+toDo+"</li>");
+    }
+})
