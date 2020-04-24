0207d2cfed08410433bf86cb1d0de5e26ba87cfb
AbdulMalik <abdumaliksharipov@gmail.com>
2020-04-24 06:25:12 -0500

    List styling2

--- toDoListjQuery/assets/js/todos.js
+++ toDoListjQuery/assets/js/todos.js
@@ -1,23 +1,23 @@
 
 
-$("ul").on("click","li",function(){
+$("ul").on("click","li", function(){
     $(this).toggleClass("completed");
 
 });
 
 
 $("ul").on("click","span",function(e){
-    $(this).parent().fadeOut(1500,function(){
-        $(this.remove());
+    $(this).parent().fadeOut(800,function(){
+        $(this).remove();
     });
 
     e.stopPropagation();
 });
 
-$("input").keypress(function(e){
-    if(e.which===13){
-        let toDo=$(this).val();
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13){
+        let toDoText = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X</span> "+toDo+"</li>");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span>" +toDoText+"</li>");
     }
-})
+});
